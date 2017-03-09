dc74a44b3a8d176dab90e067b0d0b6b3d6c1eeff
luoshiqing <644402920@qq.com>
2017-03-09 15:18:44 +0800

    first commit

---
+++ SetValuesForKeys/BBViewController.swift
@@ -0,0 +1,48 @@
+//
+//  BBViewController.swift
+//  SetValuesForKeys
+//
+//  Created by sqluo on 2017/1/20.
+//  Copyright © 2017年 sqluo. All rights reserved.
+//
+
+import UIKit
+
+class BBViewController: UIViewController {
+
+    var id: String?
+    var name: String?
+    var haha: [String:Any]?
+    var array: [Int]?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- SetValuesForKeys/ViewController.swift
+++ SetValuesForKeys/ViewController.swift
@@ -8,18 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ABC : NSObject{
+    
+    var id: String?
+    var name: String?
+    var age: String?
+    var exo: String?
 
+    //字典初始化
+    init(dict: [String:String]){
+        super.init()
+        
+        //给所有成员赋值
+        self.getMembers(of: dict)
+    }
+}
+
+extension NSObject {
+    
+    //MARK:获取类的成员属性，根据dict返回对应的dict
+    /**
+     - parameter dict:     需要设置的字典
+     */
+    public func getMembers(of dict: [String:Any]){
+        
+        var tmpDict = dict          //返回的字典
+        var members = [String]()    //存放类所有成员
+        var count: UInt32 = 0
+        let ivars = class_copyIvarList(self.classForCoder, &count) //获取有多少个成员
+        for i in 0..<Int(count) {
+            let ivar = ivars![i]
+            let name = ivar_getName(ivar)
+            let strName = String(cString: name!)
+            members.append(strName)
+        }
+        free(ivars)
+        //排查是否含有该成员
+        for (key,_) in tmpDict {
+            if !members.contains(key) {
+                print("**不包含该key**:\(key)")
+                tmpDict.removeValue(forKey: key)
+            }
+        }
+        //给所有成员赋值
+        self.setValuesForKeys(tmpDict)
+    }
+    
+}
+
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let dict = ["id":"5555","array":"4","sb":"dx","haha":"3"]
+        
+        let abc = ABC(dict: dict)
+        
+        print(abc.age ?? "age = nil")
+        print(abc.name ?? "name = nil")
+        print(abc.id ?? "id = nil")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
